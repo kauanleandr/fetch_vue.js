78518135da3a12309f7bf0f5700ebec0037ff077
kauan moreira <perry@ricardo.vigliano>
2024-10-24 16:36:36 -0300

    bo pra resolver em casa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -93,20 +93,19 @@ a {
 
 
 <template>
-
-    <h1>Dados do fetch</h1>
+  <h1>nem precisa explicar</h1>
   <div class="button1-1">
     <button @click="fetchData">CARREGAR DADOS</button>
   </div>
+  <div class="button2-2">
+    <button @click="fetch_feche">FECHAR DADOS</button>
+  </div>
+  <div class="button3-3">
+    <button @click="fetch_carregar">ENVIAR DADO</button>
+  </div>
   
   <table class="mt-5 table">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Body</th>
-      </tr>
-    </thead>
+
     <tbody>
       <tr v-for="item in items" :key="item.id">
         <td>{{ item.name }}</td>
@@ -115,47 +114,81 @@ a {
       </tr>
     </tbody>
   </table>
-  </template>
-  
-  <script>
-  export default {
-    name: 'MyComponent',
-    props: {
-      msg: String
-    },
-    data() {
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  props: {
+    msg: String
+  },
+  data() {
     return {
       items: []
     };
   },
-    methods: {
-      async fetchData() {
-        try {
-          const response = await fetch('https://jsonplaceholder.typicode.com/comments');
-          if (!response.ok) {
-            throw new Error('Erro na rede');
-          }
-          const data = await response.json();
-          console.log(data); // Exibe os dados no console
-        } catch (error) {
-          console.error('Erro ao buscar dados:', error);
-        }
+  methods: {
+    async fetch_carregar(){
+    fetch('https://jsonplaceholder.typicode.com/comments',{
+      method:'post',
+      body: JSON.stringify({
+        posId:'2037',
+        id:'234',
+        name:'paralelepipedo',
+        email:'paralelepipedo.gmail.com',
+        body:'dolor veritatis ipsum accusamus quae voluptates sint voluptatum et\nharum saepe dolorem enim\nexpedita placeat qui quidem aut et et est\nminus odit qui possimus qui saepe'
+        })
+      })
+      .then(response => response.json())
+      .then(json => console.log('form data',json));
+    },
+    async fetchData() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await response.json();
+        this.items = data;
+        console.log(this.items);
+      } catch (error) {
+        console.error;
       }
+    },
+    fetch_feche() {
+      this.items = [];
     }
   }
-  </script>
+}
+</script>
 <style>
 .imagem-xd {
     max-width: 130px;
     height: auto;
 }
 
+button {
+  font-size: 16px;
+  padding: 10px 20px;
+  margin-bottom: 10px;
+}
 .button1-1 {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: black;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+.button2-2 {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 0px;
+}
+.button3-3{
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 0px;
 }
 
 h1 {
@@ -169,11 +202,5 @@ table {
   display: flex;
   justify-content: center;
   align-items: center;
-}
-
-button {
-  font-size: 16px;
-  padding: 15px 30px;
-  margin-bottom: 30px;
 }
 </style>
