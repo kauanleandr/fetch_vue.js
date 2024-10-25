ab2aa8320a4d22d632079c0d54be8bdeebd99315
kauanleandr <kauanprograma@gmail.com>
2024-10-25 09:46:26 -0300

    dando uma polida

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,7 @@ export default {
     HelloWorld
   }
 }
-// import HelloWorld from './components/HelloWorld.vue'
-
-// export default {
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   } 
-// }
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: black;
-  margin-top: 60px;
-} */
 </style>
